c76cf014c64365c425e46dc47a951bbd4c7f0a74
Ezequiel Monachesi <ezequiel.monachesi@gmail.com>
2023-04-02 23:30:59 -0300

    ejercicio finalizado

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let comenzarJuego = document.querySelector('#comenzarJuego');
 let nRandom = 0;
 
 comenzarJuego.addEventListener('click', ()=>{
-    nRandom = Math.floor(Math.random() * 3) + 1;
+    nRandom = Math.floor(Math.random() * 5) + 1;
 } )
 
 let input = document.querySelector('input[name="numero"]');
@@ -12,12 +12,16 @@ input.addEventListener('click', ()=>{
 });
 input.addEventListener('keyup', ()=>{
     valorInput=parseInt(input.value)
-})
+});
 
 let enviar = document.querySelector('#enviar');
 enviar.addEventListener('click',()=>{
-    if(valorInput === nRandom){
-        console.log('Adivinaste el número!')
-    }else{console.log(`No adivinaste el número`)}
+    if(valorInput > nRandom){
+        alert('El número que elegiste es mayor!')
+    }else if(valorInput < nRandom){
+        alert(`El número que elegiste es menor!`)
+    }else{
+        alert(`Adivinaste el número! Felicidades!`)
+    }
 }
 )
